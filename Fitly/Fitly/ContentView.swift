251e4461a8b714790e7cf5df8d1088a2091a2987
angel zambrano <angeldzambrano99@gmail.com>
2025-03-09 13:21:20 -0700

    added fitly

---
+++ Fitly/Fitly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fitly
+//
+//  Created by angel zambrano on 3/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fitly/Fitly/FitlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitlyApp.swift
+//  Fitly
+//
+//  Created by angel zambrano on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct FitlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
